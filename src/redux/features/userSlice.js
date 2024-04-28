ca859b3986d3eb9e28d2744ee90684e8daaf2fb4
mdasik0 <asikthe1st@gmail.com>
2024-04-28 22:32:53 +0600

    async thunk created for login and register

---
+++ src/redux/features/userSlice.js
@@ -0,0 +1,110 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../../utils/firebase.config";
+
+const initialState = {
+  name: "",
+  email: "",
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+// whatever user sends as a body in the hook recived as a obj in the payload,
+// you can destructure it ({email, password}) or you can do this --
+export const createUser = createAsyncThunk(
+  "/userSlice/createUser",
+  async (payload) => {
+    const data = await createUserWithEmailAndPassword(
+      auth,
+      payload.email,
+      payload.password
+    );
+    await updateProfile(auth.currentUser, { displayName: payload.name });
+
+    console.log(data);
+    return { name: data.user.displayName, email: data.user.email };
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "/userSlice/loginUser",
+  async (payload) => {
+    const data = await signInWithEmailAndPassword(
+      auth,
+      payload.email,
+      payload.password
+    );
+    return { name: data.user.displayName, email: data.user.email };
+  }
+);
+
+const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {
+    setUser: (state, { payload }) => {
+      state.name = payload.name;
+      state.email = payload.email;
+    },
+    toggleLoading: (state, { payload }) => {
+      state.isLoading = payload;
+    },
+    resetUser: (state) => {
+      state.name = "";
+      state.email = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.name = "";
+        state.email = "";
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.name = action.payload.name;
+        state.email = action.payload.email;
+        state.isLoading = false;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.name = "";
+        state.email = "";
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.name = "";
+        state.email = "";
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.name = action.payload.name;
+        state.email = action.payload.email;
+        state.isLoading = false;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.name = "";
+        state.email = "";
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { resetUser, setUser, toggleLoading } = userSlice.actions;
+export default userSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import otherSlice from './features/otherSlice'
+import userSlice from './features/userSlice'
 
 const store = configureStore({
   reducer: {
-    otherSlice: otherSlice
+    // [userApi.reducerPath]: userApi.reducer,
+    otherSlice: otherSlice,
+    userSlice: userSlice,
   },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(userApi.middleware),
 })
 
 export default store
