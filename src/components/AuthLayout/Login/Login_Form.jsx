5c1cbb07cff75073863a0f2467764d67343cc4c9
mdasik0 <asikthe1st@gmail.com>
2024-05-04 14:07:15 +0600

    social login complete

--- src/components/AuthLayout/Login/Login_Form.jsx
+++ src/components/AuthLayout/Login/Login_Form.jsx
@@ -1,64 +1,89 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login_Form = () => {
-    const [user, setUser] = useState({ email: "", password: "", show: false });
+  const [formData, setFormData] = useState({ email: "", password: "", show: false });
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(user);
-    };
-    return (
-        <form onSubmit={handleSubmit} className="px-6">
-          <div className="text-center mb-10">
-            <h2 className="text-5xl font-bold mb-3">Welcome Back!</h2>
-            <p>please enter your email & password</p>
-          </div>
-        <div className="mb-4">
-          <label className="text-sm block mb-1" htmlFor="email">Email</label>
-          <input
-            onChange={(e) => {
-              setUser({ ...user, email: e.target.value });
-            }}
-            className="border-[2px] block w-full rounded px-2 py-1"
-            placeholder="eg@gmail.com"
-            type="email"
-            name="email"
-            id="email"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-sm  block mb-1" htmlFor="password">Password</label>
-          <input
-            onChange={(e) => {
-              setUser({ ...user, password: e.target.value });
-            }}
-            className="border-[2px] block w-full rounded px-2 py-1"
-            placeholder="**********"
-            type={user.show ? "text" : "password"}
-            name="password"
-            id="password"
-          />
-        </div>
-        
-        <div className="flex items-center justify-between mb-4">
-        <div className="flex items-center gap-1">
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = formData.email;
+    const password = formData.password;
+
+    console.log(email,password)
+  };
+  return (
+    <form onSubmit={handleSubmit} className="px-6">
+      <div className="text-center mb-10">
+        <h2 className="text-5xl font-bold mb-3">Welcome Back!</h2>
+        <p>Please enter your email & password</p>
+      </div>
+      <div className="mb-4">
+        <label className="text-sm block mb-1" htmlFor="email">
+          Email
+        </label>
         <input
-          onChange={() => {
-            setUser({ ...user, show: !user.show });
+          onChange={(e) => {
+            setFormData({ ...formData, email: e.target.value });
           }}
-          className="cursor-pointer"
-          type="checkbox"
-          name="show"
-          id="show"
-        /> <span className="text-sm">show password</span>
+          className="border-[2px] border-gray-300 block w-full  px-3 py-2 rounded-full"
+          placeholder="example@gmail.com"
+          required
+          type="email"
+          name="email"
+          id="email"
+        />
+      </div>
+      <div className="mb-4">
+        <label className="text-sm  block mb-1" htmlFor="password">
+          Password
+        </label>
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, password: e.target.value });
+          }}
+          className="border-[2px] border-gray-300 block w-full  px-3 py-2 rounded-full"
+          placeholder="########"
+          required
+          type={formData.show ? "text" : "password"}
+          name="password"
+          id="password"
+        />
+      </div>
+
+      <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center gap-1">
+          <input
+            onChange={() => {
+              setFormData({ ...formData, show: !formData.show });
+            }}
+            className="cursor-pointer"
+            type="checkbox"
+            name="show"
+            id="show"
+          />{" "}
+          <span className="text-sm">Show password</span>
         </div>
-        <span className="text-sm hover:underline hover:text-blue-500 duration-200 cursor-pointer">Forgot Password?</span>
-        </div>
-        <button type="submit" className="block bg-zinc-800 rounded w-full text-white py-2 hover:bg-zinc-700 duration-500 active:scale-90">
-          Submit
-        </button>
-      </form>
-    );
+        <span className="text-sm hover:underline hover:text-blue-500 duration-200 cursor-pointer">
+          Forgot Password?
+        </span>
+      </div>
+      <button
+        type="submit"
+        className="block bg-zinc-800 font-semibold w-full text-white py-2.5 rounded-full hover:bg-zinc-700 duration-500 active:scale-90 mb-3"
+      >
+        Submit
+      </button>
+      <span className="text-sm w-full flex items-center gap-1 justify-end ">
+        Don&apos;t have an Account?{" "}
+        <Link
+          to={`/auth/sign-up`}
+          className="hover:underline hover:text-blue-500 duration-200 cursor-pointer"
+        >
+          Register!
+        </Link>{" "}
+      </span>
+    </form>
+  );
 };
 
 export default Login_Form;
--- src/components/AuthLayout/Login/Login_FormSection.jsx
+++ src/components/AuthLayout/Login/Login_FormSection.jsx
@@ -1,17 +1,18 @@
+import SocialLogin from "../SocialLogin";
 import Login_Form from "./Login_Form";
 
 const Login_FormSection = () => {
   return (
     <div className="h-5/6 w-1/3 ml-16">
       <div className="flex justify-between items-center mb-14">
-        <figure className="font-bold text-4xl ">Logo</figure>
+        <figure className="font-bold text-4xl ml-6">Logo</figure>
         <div>
         </div>
       </div>
       <Login_Form />
       <div className="divider text-sm my-10">OR</div>
-      <div>
-        Google Login
+      <div className="flex justify-center">
+        <SocialLogin />
       </div>
     </div>
   );
