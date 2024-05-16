01e3413058892f7127e42af17c844a728a8e070e
mdasik0 <asikthe1st@gmail.com>
2024-05-16 12:14:05 +0600

    some testing on useStatus

--- src/pages/HomeLayout/Chats.jsx
+++ src/pages/HomeLayout/Chats.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "../../components/Shared/Sidebar";
 
 const Chats = () => {
-    return (
-        <div className="flex">
-            <Sidebar />
-            here is your all the chats
-        </div>
-    );
+  return (
+    <div className="flex w-full">
+      <Sidebar />
+      <div className="w-full">
+      here is your all the chats
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/pages/HomeLayout/VideoCall.jsx
+++ src/pages/HomeLayout/VideoCall.jsx
@@ -1,12 +1,77 @@
+import { useEffect, useState } from "react";
 import Sidebar from "../../components/Shared/Sidebar";
 
 const VideoCall = () => {
-    return (
-        <div className="flex">
-            <Sidebar />
-            join meeting
-        </div>
-    );
+  const [nothing, setnothing] = useState(null);
+  const something = [
+    {
+      id: 1,
+      title: "i am no 1",
+      status: "pending",
+    },
+    {
+      id: 2,
+      title: "i am no 2",
+      status: "pending",
+    },
+    {
+      id: 3,
+      title: "i am no 3",
+      status: "pending",
+    },
+    {
+      id: 4,
+      title: "i am no 4",
+      status: "pending",
+    },
+    {
+      id: 5,
+      title: "i am no 5",
+      status: "pending",
+    },
+  ];
+
+  useEffect(() => {
+    setnothing(something);
+  }, []);
+
+  const handleDelete = (id) => {
+    const filter = nothing?.find((x) => x.id === id);
+
+    if (filter) {
+        const statusUpdate = { ...filter, status: "active" };
+        console.log(statusUpdate);
+
+        // Create a new array with the updated object
+        const updatedNothing = nothing.map((item) => 
+            item.id === id ? statusUpdate : item
+        );
+
+        // Update the state
+        setnothing(updatedNothing);
+    }
+};
+
+  useEffect(() => {
+    console.log(nothing);
+  }, [nothing]);
+
+  const pending = nothing?.filter((y) => y.status === "pending");
+  return (
+    <div className="flex w-full">
+      <Sidebar />
+      <div className="w-full flex gap-4 items-center justify-center">
+        {pending?.map((i) => (
+          <div className="bg-green-500 w-fit" key={i.id}>
+            <h1>{i.title}</h1>
+            <button onClick={() => handleDelete(i.id)} className="bg-red-500">
+              done
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default VideoCall;
