cea3519170b9b5835316faba248b8797361de254
mdasik0 <asikthe1st@gmail.com>
2024-09-26 23:46:17 +0600

    sustkp

--- src/components/HomeLayout/Homepage/Navbar.jsx
+++ src/components/HomeLayout/Homepage/Navbar.jsx
@@ -1,50 +1,8 @@
-import { GoHome } from "react-icons/go";
-import { FaExclamation } from "react-icons/fa";
-import logo from "/logo/logo-white.png";
-import ProjectsNav from "./ProjectsNav";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import UserModal from "./UserModal";
-const Navbar = () => {
-  const userInfo = useSelector((state) => state.userSlice);
-  const user = userInfo.email || null;
-  const { isLoading } = userInfo;
 
+const Navbar = () => {
   return (
-    <div className="flex justify-between items-start px-20 py-6">
-      {/* logo */}
-      <div>
-        <img className="h-8 w-fit" src={logo} alt="" />
-      </div>
-      <ul className="flex gap-3 items-start">
-        <li className="flex items-center bg-gray-200 rounded-full p-1 cursor-pointer hover:bg-gray-300 duration-500">
-          <div className="h-7 w-7 bg-white flex items-center justify-center rounded-full ">
-            <GoHome />{" "}
-          </div>
-          <span className="ps-1.5 pe-3">Home</span>
-        </li>{" "}
-        <li className="flex items-center bg-gray-200 rounded-full p-1 cursor-pointer hover:bg-gray-300 duration-500">
-          <div className="h-7 w-7 bg-white flex items-center justify-center rounded-full">
-            <FaExclamation />{" "}
-          </div>
-          <span className="ps-1.5 pe-3">About</span>
-        </li>{" "}
-        <ProjectsNav />
-      </ul>
-      {user ? (
-        <UserModal userInfo={userInfo} user={user} />
-      ) : (
-        <Link
-          to={"/auth/sign-in"}
-          className="login-button  cursor-pointer px-6 py-1.5 rounded-full"
-        >
-          {isLoading ? (
-            <span className="loading loading-spinner loading-sm"></span>
-          ) : (
-            "Login"
-          )}
-        </Link>
-      )}
+    <div>
+      
     </div>
   );
 };
