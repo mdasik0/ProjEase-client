4b975e0a4099c04ce65b5eff53bcca53cd499075
mdasik0 <asikthe1st@gmail.com>
2024-09-27 20:58:33 +0600

    nav planning completed

--- src/components/HomeLayout/Homepage/Navbar.jsx
+++ src/components/HomeLayout/Homepage/Navbar.jsx
@@ -1,9 +1,20 @@
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div>
+        {/* logo here */}
+      </div>
+      <ul>
+        {/* nav links here */}
+        {/* home */}
+        {/* about */}
+        {/* projects */}
+      </ul>
+      <div>
+        {/* user img and user dashboard is here */}
+      </div>
+    </nav>
   );
 };
 
