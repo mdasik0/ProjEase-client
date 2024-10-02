3a4a396cbe108169e92791aa18bc68572ab7bf4f
mdasik0 <asikthe1st@gmail.com>
2024-10-02 22:04:00 +0600

    tab responsive done

--- src/components/HomeLayout/Homepage/Header.jsx
+++ src/components/HomeLayout/Homepage/Header.jsx
@@ -1,17 +1,17 @@
-import headerImage from "../../../../public/header-img/header-img-save-file-comp.png";
-import star from "../../../../public/header-img/star.png";
+import headerImage from "/header-img/header-img-save-file-comp.png";
+import star from "/header-img/star.png";
 const Header = () => {
   return (
-    <header className="flex justify-between items-center max-w-[90vw] mx-auto mt-10">
-      <div>
-        <h1 className="text-6xl font-bold leading-[80px]">
+    <header className="flex justify-between items-center max-w-[90vw] mx-auto mt-10 gap-10">
+      <div className=" lg:w-[500px] ">
+        <h1 className="lg:text-6xl text-4xl leading-[50px] font-bold lg:leading-[80px]">
           <span className="flex items-center gap-4">
-            Simplify <img className="w-10 h-10" src={star} alt="star img" />
+            Simplify <img className="lg:w-10 md:w-6 lg:h-10 md:h-6" src={star} alt="star img" />
           </span>{" "}
           Your Project <p>Management .</p>
         </h1>
-        <p className="w-7/12 my-6 text-gray-600">ProjEase lets you manage all your projects in one place. With built-in tasks, chat, and video calls, there’s no need to switch between tools.</p>
-        <div className="flex bg-white rounded-full p-[2px] w-6/12 border border-gray-300">
+        <p className=" lg:my-6 md:my-4 text-sm text-gray-600">ProjEase lets you manage all your projects in one place. With built-in tasks, chat, and video calls, there’s no need to switch between tools.</p>
+        <div className="flex bg-white rounded-full p-[2px] lg:mr-10 border border-gray-300 lg:text-base md:text-sm">
           <input className="flex-grow px-3 focus:outline-none bg-transparent " type="email" placeholder="Enter your email" />
           <button className="bg-[#1a1a1a]  text-white px-4 py-2 rounded-full" type="submit">Get Started </button>
         </div>
