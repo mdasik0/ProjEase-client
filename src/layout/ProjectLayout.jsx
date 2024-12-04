0d7d4d4230b152164bdd49bcf765a341c0012f42
mdasik0 <asikthe1st@gmail.com>
2024-12-04 10:53:16 +0600

    project data stored in project Slice and can be distributed to all project routes

--- src/layout/ProjectLayout.jsx
+++ src/layout/ProjectLayout.jsx
@@ -1,17 +1,39 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProjectAction from "../components/ProjectLayout/ProjectAction";
 import Project_sidebar from "../components/Shared/Project_sidebar";
 import { Outlet } from "react-router-dom";
+import { useGetProjectQuery } from "../redux/api/projectsApi";
+import { useEffect } from "react";
+import { storeActiveProject } from "../redux/features/projectSlice";
 
 const ProjectLayout = () => {
-  const { userData } = useSelector((state) => state.userSlice);
+  const { userData, isLoading } = useSelector((state) => state.userSlice);
 
   const joinedProjects = userData?.joinedProjects;
 
   // if projects is available then show the project dashboard
   // if the project is not available then show create or join a project hyperlink
 
+  const dispatch = useDispatch();
+  console.log();
+  const activeProjectId = joinedProjects?.find(
+    (p) => p.status === "active"
+  ).projectId;
 
+  const { data } = useGetProjectQuery(activeProjectId);
+
+  useEffect(() => {
+    if (data) {
+      dispatch(storeActiveProject(data));
+    }
+  }, [data]);
+  if (isLoading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+    );
+  }
   if (joinedProjects) {
     return (
       <div className="flex">
--- src/pages/ProjectLayout/ProjectsDashboard.jsx
+++ src/pages/ProjectLayout/ProjectsDashboard.jsx
@@ -1,14 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
 
 const ProjectsDashboard = () => {
-const dispatch = useDispatch();
-    const {userData} = useSelector((state) => state.userSlice)
-
-    console.log();
-    const activeProjectId = userData.joinedProjects.find(p => p.status === 'active').projectId;
-console.log(activeProjectId);
-    // now make a api end point that will find specific project with a project id;
-
 
 
     return (
