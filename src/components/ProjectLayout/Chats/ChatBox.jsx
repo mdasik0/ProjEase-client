a6471fcb65200d5893cbd6832935de01911da864
mdasik0 <asikthe1st@gmail.com>
2025-01-03 05:50:20 +0600

    others chat card created

--- src/components/ProjectLayout/Chats/ChatBox.jsx
+++ src/components/ProjectLayout/Chats/ChatBox.jsx
@@ -1,24 +1,36 @@
 import { LuReplyAll } from "react-icons/lu";
+import OthersChatCard from "./OthersChatCard";
 
 const ChatBox = () => {
   return (
     <div className="flex-grow scrollbar mx-8 overflow-y-scroll">
-      <div className="chat-card-container max-w-[45%] min-w-[10%]">
-        <div className="avatar&time flex items-end justify-between ">
-          <div className="user-avatar w-8 h-8 rounded-full bg-blue-500">
-          </div>
-          <div className="time text-xs">
-            12:30 am 
-          </div>
-        </div>
-        <div className="message text-[15px] bg-gray-200 w-fit p-2 px-3.5 rounded-xl rounded-tl-none mt-2">
-            Lorem ipsum dolor sit amet, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores pariatur distinctio placeat totam quibusdam culpa expedita rem quisquam veritatis ut.
-        </div>
-        <div className="reply&Action mt-3">
-            <button className="bg-gray-200 px-3 pe-4 py-1 rounded-full flex items-center gap-2"> <LuReplyAll />Reply</button>
-            <button></button>
-        </div>
-      </div>
+
+
+      <OthersChatCard />
+
+      <div className="chat-card-container block max-w-[50%] min-w-[10%]">
+                      <div className="w-fit">
+              
+                          <div className="avatar&time flex items-end justify-between ">
+                            <div className="user-avatar w-8 h-8 rounded-full bg-blue-500"></div>
+                            <div className="time text-xs">12:30 am</div>
+                          </div>
+              
+                          <div className="message-actions-container relative">
+                            {" "}
+                            <p className="message text-[15px] bg-gray-200  p-2 px-3.5 rounded-xl rounded-tl-none mt-2">Lorem ipsum </p>
+                            <div className="replyandActions">
+                              <button
+                                className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 duration-300 tooltip absolute top-[50%] -translate-y-[50%] -right-[40px]"
+                                data-tip="reply"
+                              >
+                                <LuReplyAll />
+                              </button>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+      
     </div>
   );
 };
