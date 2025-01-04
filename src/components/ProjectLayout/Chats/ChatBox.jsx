244be20f6418a873234381f22ecc787c0b8c2367
mdasik0 <asikthe1st@gmail.com>
2025-01-04 08:31:20 +0600

    chatbox messages done now need to show it in the ui

--- src/components/ProjectLayout/Chats/ChatBox.jsx
+++ src/components/ProjectLayout/Chats/ChatBox.jsx
@@ -1,7 +1,27 @@
 import OthersChatCard from "./OthersChatCard";
 import MyChatCard from "./MyChatCard";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import toast from "react-hot-toast";
+const ChatBox = ({socket}) => {
+  const [messages, setMessages] = useState([]);
 
-const ChatBox = () => {
+  useEffect(() => {
+    socket.on("groupMessageReceived", (data) => {
+      console.log(data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+    socket.on("error", (data) => {
+      toast.error(data.message);
+    });
+    return () => {
+      socket.off("groupMessageReceived")
+      socket.off("error")
+    }
+  }, [])
+
+  console.log(messages);
+
   return (
     <div className="flex-grow scrollbar ms-8 me-4 pr-3 overflow-y-scroll overflow-x-hidden">
       <OthersChatCard />
@@ -9,5 +29,13 @@ const ChatBox = () => {
     </div>
   );
 };
+
+ChatBox.propTypes = ({
+  socket: PropTypes.shape({
+      emit: PropTypes.func.isRequired,
+      on: PropTypes.func,
+      off: PropTypes.func,
+    }).isRequired,
+})
 
 export default ChatBox;
--- src/pages/HomeLayout/Chats.jsx
+++ src/pages/HomeLayout/Chats.jsx
@@ -1,65 +1,56 @@
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
 import ChatsHeader from "../../components/ProjectLayout/Chats/ChatsHeader";
 import { useGetMultiUserQuery } from "../../redux/api/userApi";
 import ChatBox from "../../components/ProjectLayout/Chats/ChatBox";
-import { FaArrowAltCircleRight, FaPaperPlane } from "react-icons/fa";
 import SendChatMessage from "../../components/ProjectLayout/Chats/SendChatMessage";
+import toast from "react-hot-toast";
 
 const socket = io("http://localhost:5000");
 
 const Chats = () => {
-  const [userId, setUserId] = useState("");
-  const [groupId, setGroupId] = useState("");
-  const [messages, setMessages] = useState([]);
-
   const { projectData } = useSelector((state) => state.projectSlice);
   const { userData } = useSelector((state) => state.userSlice);
-  // useEffect(() => {
-  //   if (userData?._id) {
-  //     socket.emit("register", userData?._id);
-  //   }
 
-  //   socket.on("registerResponse", (data) => {
-  //     if (data.success) {
-  //       toast.success(data.message);
-  //     } else {
-  //       toast.error(data.message);
-  //     }
-  //   });
+  useEffect(() => {
+    //users registration
+    if (userData?._id) {
+      socket.emit("register", userData?._id);
+    }
 
-  //   if (projectData?._id) {
-  //     socket.emit("joinGroup", projectData?._id);
-  //   }
+    socket.on("registerResponse", (data) => {
+      if (data.success) {
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    });
 
-  //   socket.on("groupJoinResponse", (data) => {
-  //     console.log(data);
-  //     toast.success(data.message);
-  //   });
+    //group registration
+    if (projectData?._id) {
+      socket.emit("joinGroup", projectData?._id);
+    }
 
-  //   socket.on("groupMessageReceived", (data) => {
-  //     console.log(data);
-  //     setMessages((prevMessages) => [...prevMessages, data]);
-  //   });
+    socket.on("groupJoinResponse", (data) => {
+      console.log(data);
+      toast.success(data.message);
+    });
 
-  //   socket.on("error", (data) => {
-  //     toast.error(data.message);
-  //   });
+    socket.on("error", (data) => {
+      toast.error(data.message);
+    });
 
-  //   return () => {
-  //     socket.off("registerResponse");
-  //     socket.off("groupJoinResponse");
-  //     socket.off("groupMessageReceived");
-  //     socket.off("error");
-  //   };
-  // }, [projectData?._id]); // Add projectData in the dependency array
+    return () => {
+      socket.off("registerResponse");
+      socket.off("groupJoinResponse");
+      socket.off("error");
+    };
+  }, [projectData?._id]); // Add projectData in the dependency array
 
   const membersIDs = projectData?.members?.map((m) => m.userId);
 
   const { data: members } = useGetMultiUserQuery(membersIDs);
-  // console.log(data);
 
   const openChatSettingModal = () => {};
 
@@ -70,7 +61,7 @@ const Chats = () => {
         projectData={projectData}
         members={members}
       />
-      <ChatBox />
+      <ChatBox socket={socket} />
       <SendChatMessage groupChatId={projectData?._id} socket={socket} />
     </div>
   );
