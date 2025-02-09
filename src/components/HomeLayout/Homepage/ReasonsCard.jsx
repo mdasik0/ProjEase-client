6cffce4df398868e81b6ea4713a1f0cba25321cc
mdasik0 <asikthe1st@gmail.com>
2025-02-09 07:33:10 +0600

    small changes - 2

--- src/components/HomeLayout/Homepage/ReasonsCard.jsx
+++ src/components/HomeLayout/Homepage/ReasonsCard.jsx
@@ -5,7 +5,7 @@ const ReasonsCard = ({
   children = "N/A",
 }) => {
   return (
-    <div className="the-card flex items-start gap-1 mb-3 h-[120px] bg-[#1a1a1a] text-white rounded-2xl">
+    <div className="the-card flex items-start gap-1 mb-3 h-[120px] bg-[#1a1a1a] hover:bg-[#0f0f0f] duration-300 text-white rounded-2xl cursor-pointer">
       <div className="front flex items-center gap-6 h-full w-full p-3 rounded-xl overflow-hidden -400">
         <div className="no text-6xl ml-4">{index}</div>
         <div className="w-[2px] h-[70px] bg-gray-500 "></div>
