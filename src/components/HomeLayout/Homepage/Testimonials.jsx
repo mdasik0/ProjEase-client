a90c4ad88c700f23fb1483073798d1245a90e25b
mdasik0 <asikthe1st@gmail.com>
2025-02-11 14:57:50 +0600

    testimonial slider card complete

--- src/components/HomeLayout/Homepage/Testimonials.jsx
+++ src/components/HomeLayout/Homepage/Testimonials.jsx
@@ -4,6 +4,7 @@ const Testimonials = () => {
         <div className="max-w-[90vw] mx-auto my-20">
             <p className="text-gray-500 font-[500] text-center mb-6">Testimonials</p>
             <h1 className="text-center text-5xl font-[500]">What Our Users has to say about us?</h1>
+            
         </div>
     );
 };
