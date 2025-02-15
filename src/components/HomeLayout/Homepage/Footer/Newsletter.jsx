daefc263d20c15fb42505e691f6c52387b27f892
mdasik0 <asikthe1st@gmail.com>
2025-02-15 18:39:11 +0600

    Homepage is fully responsive

--- src/components/HomeLayout/Homepage/Footer/Newsletter.jsx
+++ src/components/HomeLayout/Homepage/Footer/Newsletter.jsx
@@ -3,9 +3,9 @@ const Newsletter = () => {
     return (
         <div className="newsletter uppercase bg-no-repeat bg-cover bg-center py-10 w-full flex md:flex-row flex-col justify-center items-center md:gap-10 gap-4">
         <p className="text-white">Subscribe to our weekly newsletter</p>
-        <div className=" p-1 rounded-full bg-gray-100 ">
+        <div className=" p-1 rounded-full bg-gray-100 flex items-center">
           <input
-            className="px-3 py-2 bg-transparent w-[300px]"
+            className="px-3 py-2 bg-transparent md:w-[300px]"
             type="email"
             placeholder="Enter your email"
           />
