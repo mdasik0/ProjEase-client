411538fcb9fa39606a5c6708bacc41cd70833835
mdasik0 <asikthe1st@gmail.com>
2025-02-24 21:25:20 +0600

    project dashboard last update

--- src/pages/ProjectLayout/ProjectsDashboard.jsx
+++ src/pages/ProjectLayout/ProjectsDashboard.jsx
@@ -1,12 +1,22 @@
-
 const ProjectsDashboard = () => {
-
-
-    return (
-        <div>
-            project dashboard
-        </div>
-    );
+  // work on dashboard layout
+  // decide what to fill on those boxes and fill them one by one
+  // add functionality
+  // done
+  // next move to user dashboard 
+  return (
+    <div className="p-10 bg-green-200 w-full h-screen">
+      <div className="search"></div>
+      <div className="w-full grid grid-cols-3 gap-3">
+        <div className="1 h-18 bg-red-300"></div>
+        <div className="2 h-20 bg-red-300"></div>
+        <div className="3 h-24 bg-red-300"></div>
+        <div className="3 h-22 bg-red-300"></div>
+        <div className="3 h-20 bg-red-300"></div>
+        <div className="3 h-14 bg-red-300"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsDashboard;
