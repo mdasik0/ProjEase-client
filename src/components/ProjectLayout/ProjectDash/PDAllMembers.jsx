bf69fa9775edf4f28e6af16a2f9a266c5a372528
Md asik <asikthe1st@gmail.com>
2025-03-25 12:31:41 +0600

    Enhance PDAllMembers component to fetch and display project members using PDMembersCard; add PropTypes for validation

--- src/components/ProjectLayout/ProjectDash/PDAllMembers.jsx
+++ src/components/ProjectLayout/ProjectDash/PDAllMembers.jsx
@@ -1,16 +1,33 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import PDMembersCard from "./PDMembersCard";
+import { useGetMultiUserQuery } from "../../../redux/api/userApi";
 
 const PDAllMembers = () => {
+    const { projectData } = useSelector(state => state.projectSlice);
+
+    const AllMembersUserId = useMemo(() => 
+        projectData?.members?.map(m => m.userId) || [], 
+        [projectData]
+    );
+
+    const { data } = useGetMultiUserQuery(AllMembersUserId);
+
     return (
         <div className="border border-gray-300 rounded-lg py-3 ps-3 pe-2 w-2/5">
-              <h3 className="font-[500] mb-2 text-lg">Members</h3>
-              <div className="flex flex-col gap-3 h-[275px] overflow-y-auto pr-2">
-                {/* Mambers Card */}
-                <PDMembersCard />
-                
-                
-              </div>
+            <h3 className="font-[500] mb-2 text-lg">Members</h3>
+            <div className="flex flex-col gap-3 h-[275px] overflow-y-auto pr-2">
+                {/* Members Card */}
+                {data?.map(({ _id, image, name, email }) => (
+                    <PDMembersCard 
+                        key={_id} 
+                        image={image} 
+                        name={`${name?.firstname} ${name?.lastname}`} 
+                        email={email} 
+                    />
+                ))}
             </div>
+        </div>
     );
 };
 
--- src/components/ProjectLayout/ProjectDash/PDMembersCard.jsx
+++ src/components/ProjectLayout/ProjectDash/PDMembersCard.jsx
@@ -1,19 +1,26 @@
 import avatar from "/avatar/avatar.png";
+import PropTypes from "prop-types";
 
-const PDMembersCard = () => {
+const PDMembersCard = ({image, name, email}) => {
   return (
     <div className="bg-gray-100 border border-gray-300 rounded-xl p-2 flex  gap-4">
       <img
         className="w-12 h-12 bg-gray-300 rounded-full object-cover"
-        src={avatar}
+        src={image || avatar}
         alt="Members Img"
       />
       <div>
-        <h3 className="font-[500] ">John Doe</h3>
-        <p className="text-sm text-gray-500">Web Developer</p>
+        <h3 className="font-[500] ">{name}</h3>
+        <p className="text-sm text-gray-500">{email}</p>
       </div>
     </div>
   );
+};
+
+PDMembersCard.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
 };
 
 export default PDMembersCard;
