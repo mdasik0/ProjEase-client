2ea0a3fc4d28251b4caad5d6497f74814a9742e9
Md asik <asikthe1st@gmail.com>
2025-06-20 12:41:26 +0600

    chore : env change happened

--- src/redux/features/projectSlice.js
+++ src/redux/features/projectSlice.js
@@ -1,35 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-projectData: {
-    _id: '',
-},
-members: []
-}
+  projectData: {
+    _id: "",
+  },
+  members: [],
+};
 
 const projectSlice = createSlice({
-    name: 'projectSlice',
-    initialState,
-    reducers: {
-    storeActiveProject: (state, {payload}) => {
-        state.projectData = payload;
-    }, 
-    storeMembersInfo: (state, {payload}) => {
-        state.members = payload;
+  name: "projectSlice",
+  initialState,
+  reducers: {
+    storeActiveProject: (state, { payload }) => {
+      state.projectData = payload;
+    },
+    storeMembersInfo: (state, { payload }) => {
+      state.members = payload;
     },
     resetProjSlice: (state) => {
-        state.members = [];
-        state.projectData = {};
-    }, 
-    updateProjectId: (state, {payload}) => {
-        state.projectData._id = payload;
-    }
-    }
-})
+      state.members = [];
+      state.projectData = {};
+    },
+    updateProjectId: (state, { payload }) => {
+      state.projectData._id = payload;
+    },
+  },
+});
 
 // active project from userSlice.userData
 // then fetch the project
 // then store the project in the projectSlice
 
-export const {storeActiveProject, storeMembersInfo,resetProjSlice, updateProjectId} = projectSlice.actions;
+export const {
+  storeActiveProject,
+  storeMembersInfo,
+  resetProjSlice,
+  updateProjectId,
+} = projectSlice.actions;
 export default projectSlice.reducer;
